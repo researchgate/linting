eae24f39cbffc60207bfa20dfd1770e69ba76b13
Daniel Tschinder <daniel.tschinder@researchgate.net>
2017-09-02 19:47:33 +0200

    feat(eslint-base): Add no-labels rule

--- packages/eslint-config-rg-base/index.js
+++ packages/eslint-config-rg-base/index.js
@@ -73,6 +73,7 @@ module.exports = {
         'no-floating-decimal': 'error',
         'no-implied-eval': 'error',
         'no-iterator': 'error',
+        'no-labels': 'error',
         'no-lone-blocks': 'error',
         'no-loop-func': 'error',
         'no-multi-spaces': 'error',
